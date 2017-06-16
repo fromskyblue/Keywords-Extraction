1d9bd444516a0f17e5c93afe1291e506f7b43326
lichaoyang <lichaoyang@jozzon.com>
2017-06-16 11:06:29 +0800

    website 初始化

---
+++ website/website.py
@@ -0,0 +1,57 @@
+import json
+
+import pandas as pd
+from flask import Flask
+from flask import render_template, request
+
+from algorithm.keyword.keywords import get_keyword, clean_words, remove_stop, english_stop_words
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index1.html')
+
+
+@app.route('/ajax/')
+def ajax():
+    return render_template('index1.html')
+
+
+@app.route('/add_numbers/', methods=['POST'])
+def add_numbers():
+    text = request.form['file']
+    clean = clean_words(text)
+    print(clean)
+    result = get_keyword(remove_stop(clean))
+    result = dict(result[:30])
+    print(result)
+    v = pd.cut([abs(x) for x in list(result.values())], 5, retbins=True, labels=[1, 2, 3, 5, 10])[0]
+    show1 = ""
+    result = dict(zip(result.keys(), v))
+    for key, value in result.items():
+        print(key, value)
+        show1 += "<span data-weight=\" " + str(value) + " \">" + key + "</span>";
+
+    show2 = ""
+    for word in clean:
+        if word in english_stop_words:
+            show2 += "<span class=\"label label-w01\">" + word + "</span>";
+        elif word in result.keys():
+            if result[word] != 1:
+                show2 += "<span class=\"label label-w" + str(result[word]) + "\">" + word + "</span>";
+        else:
+            show2 += "<span class=\"label label-w0\">" + word + "</span>";
+
+    return json.dumps({'v1': show1, 'v2': show2})
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    print(request.form['article'])
+    return 0
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
